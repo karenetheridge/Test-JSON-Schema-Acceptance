dc060e263a784392f40d627b07a81a2a08b5065b
Karen Etheridge <ether@cpan.org>
2020-04-29 12:07:04 -0700

    fix type check in sample validator

--- t/lib/SchemaParser.pm
+++ t/lib/SchemaParser.pm
@@ -15,7 +15,7 @@ sub new {
 sub validate_data {
     my ($self, $data, $schema) = @_;
 
-    return $schema if not ref $schema;
+    return $schema if ref $schema eq 'JSON::PP::Boolean';
     die 'unrecognized schema type '.ref $schema if ref $schema ne 'HASH';
 
     return 1 if not exists $schema->{type} or ref $schema->{type} eq 'HASH';
