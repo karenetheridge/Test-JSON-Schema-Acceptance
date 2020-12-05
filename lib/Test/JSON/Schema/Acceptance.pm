03974cd46e246d0faf5fe2fc25e4666e0f09a371
Karen Etheridge <ether@cpan.org>
2020-12-04 23:12:45 -0800

    increment $VERSION after 1.003 release

--- lib/Test/JSON/Schema/Acceptance.pm
+++ lib/Test/JSON/Schema/Acceptance.pm
@@ -4,7 +4,7 @@ package Test::JSON::Schema::Acceptance;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Acceptance testing for JSON-Schema based validators like JSON::Schema
 
-our $VERSION = '1.003';
+our $VERSION = '1.004';
 
 use 5.014;
 no if "$]" >= 5.031009, feature => 'indirect';
