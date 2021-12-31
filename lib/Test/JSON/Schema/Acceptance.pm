976299c32a73f8481ae3236f8e110c7a31c4c44f
Karen Etheridge <ether@cpan.org>
2021-12-31 10:01:30 -0800

    rename internal attributes _json_decoder, _json_encoder so they are no longer private
    The underscore versions live on as backcompat shims

--- lib/Test/JSON/Schema/Acceptance.pm
+++ lib/Test/JSON/Schema/Acceptance.pm
@@ -135,7 +135,7 @@ sub acceptance {
     $self->additional_resources->visit(
       sub ($path, @) {
         return if not $path->is_file or $path !~ /\.json$/;
-        my $data = $self->_json_decoder->decode($path->slurp_raw);
+        my $data = $self->json_decoder->decode($path->slurp_raw);
         my $file = $path->relative($self->additional_resources);
         my $uri = $base.'/'.$file;
         $options->{add_resource}->($uri => $data);
@@ -187,10 +187,10 @@ sub acceptance {
         my $result = $options->{validate_data}
           ? $options->{validate_data}->($metaspec_uri, $test_group->{schema})
           # we use the decoder here so we don't prettify the string
-          : $options->{validate_json_string}->($metaspec_uri, $self->_json_decoder->encode($test_group->{schema}));
+          : $options->{validate_json_string}->($metaspec_uri, $self->json_decoder->encode($test_group->{schema}));
         if (not $result) {
           $ctx->fail('schema for '.$one_file->{file}.': "'.$test_group->{description}.'" fails to validate against '.$metaspec_uri.':');
-          $ctx->note($self->_json_encoder->encode($result));
+          $ctx->note($self->json_encoder->encode($result));
           $schema_fails = 1;
         }
       }
@@ -258,15 +258,16 @@ sub _run_test ($self, $one_file, $test_group, $test, $options) {
     sub {
       my ($result, $schema_before, $data_before, $schema_after, $data_after);
       try {
-        ($schema_before, $data_before) = map $self->_json_decoder->encode($_),
+        # we use the decoder here so we don't prettify the string
+        ($schema_before, $data_before) = map $self->json_decoder->encode($_),
           $test_group->{schema}, $test->{data};
 
         $result = $options->{validate_data}
           ? $options->{validate_data}->($test_group->{schema}, $test->{data})
             # we use the decoder here so we don't prettify the string
-          : $options->{validate_json_string}->($test_group->{schema}, $self->_json_decoder->encode($test->{data}));
+          : $options->{validate_json_string}->($test_group->{schema}, $self->json_decoder->encode($test->{data}));
 
-        ($schema_after, $data_after) = map $self->_json_decoder->encode($_),
+        ($schema_after, $data_after) = map $self->json_decoder->encode($_),
           $test_group->{schema}, $test->{data};
 
         my $ctx = Test2::API::context;
@@ -356,7 +357,7 @@ sub _mutation_check ($self, $data) {
 }
 
 # used for internal serialization also
-has _json_decoder => (
+has json_decoder => (
   is => 'ro',
   isa => HasMethods[qw(encode decode)],
   lazy => 1,
@@ -364,16 +365,20 @@ has _json_decoder => (
 );
 
 # used for pretty-printing diagnostics
-has _json_encoder => (
+has json_encoder => (
   is => 'ro',
   isa => HasMethods['encode'],
   lazy => 1,
   default => sub {
-    my $encoder = shift->_json_decoder->convert_blessed->canonical->pretty;
+    my $encoder = shift->json_decoder->convert_blessed->canonical->pretty;
     $encoder->indent_length(2) if $encoder->can('indent_length');
     $encoder;
   },
 );
+
+# backcompat shims
+sub _json_decoder { shift->json_decoder(@_) }
+sub _json_encoder { shift->json_encoder(@_) }
 
 # see JSON::MaybeXS::is_bool
 my $json_bool = InstanceOf[qw(JSON::XS::Boolean Cpanel::JSON::XS::Boolean JSON::PP::Boolean)];
@@ -407,7 +412,7 @@ sub _build__test_data ($self) {
       return if any { $self->test_dir->child($_)->subsumes($path) } $self->skip_dir->@*;
       return if not $path->is_file;
       return if $path !~ /\.json$/;
-      my $data = $self->_json_decoder->decode($path->slurp_raw);
+      my $data = $self->json_decoder->decode($path->slurp_raw);
       return if not @$data; # placeholder files for renamed tests
       my $file = $path->relative($self->test_dir);
       push @test_groups, [
